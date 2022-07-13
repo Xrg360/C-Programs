f7a9936808eb63cade52e893fcc56335102f40eb
Rohit George <rohitgeorge@gmail.com>
2022-07-13 18:16:50 +0530

    string$

--- string$.c
+++ string$.c
@@ -1,27 +1,20 @@
+//Read a string (ending with a $ symbol), store it in an array, and count the number of vowels, consonants, and spaces in it
 #include<stdio.h>
 #include<string.h>
 void main(){
-    char str[100],str1[100];
-    int i=0;
-    char ch,ch1;
-    while(ch!='$'){
-        ch=getchar();
-        str[i]=ch;
-        i++;
+    char str[100];
+    int i,vowels=0,consonants=0,spaces=0;
+    printf("Enter the string: ");
+    scanf("%s",str);
+    for(i=0;str[i]!='$';i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+            vowels++;
+        else if(str[i]==' ')
+            spaces++;
+        else
+            consonants++;
     }
-    str[i]='\0';
-    printf("%s\n",str);
- i=0;
-     while(ch1!='$'){
-        ch1=getchar();
-        str1[i]=ch1;
-        i++;
-    }
-    str1[i]='\0';
-    int m = strlen(str);
-    int n = strlen(str1);
-    int j;
-    for(i=0, j=m-1;i<n-2,j<m+n-1;i++,j++)
-        str[j]=str1[i];
-    printf("%s",str);
+    printf("The number of vowels is: %d\n",vowels);
+    printf("The number of consonants is: %d\n",consonants);
+    printf("The number of spaces is: %d\n",spaces);
 }
